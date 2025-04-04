8ff1aae0da8b2e0bb9115fe70104c19713d572a3
Emrah Burak Kaya <emrhbrkky@outlook.com>
2025-04-04 15:55:11 +0300

    ListType ve SetType'ın kullanımı.

---
+++ StackExchangeRedisApp/Controllers/ListTypeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using StackExchangeRedisApp.Services;
+
+namespace StackExchangeRedisApp.Controllers
+{
+    public class ListTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+
+        private readonly IDatabase db;
+
+        private string listKey = "names"; // Redis'te kullanacağımız anahtar.
+        public ListTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(0); //Redis 1.veritabanına bağlanıyoruz.
+        }
+
+        public IActionResult Index()
+        {
+            List<string> namesList = new List<string>();
+
+            if(db.KeyExists(listKey)) // Redis'te anahtar var mı kontrol ediyoruz.
+            {
+                var redisList = db.ListRange(listKey); // Redis'ten listeyi alıyoruz.
+                foreach (var item in redisList)
+                {
+                    namesList.Add(item.ToString()); // Liste elemanlarını string'e çevirip ekliyoruz.
+                }
+
+                //Alternatif foreach yapısı
+                //db.ListRange(listKey).ToList().ForEach(x =>
+                //{
+                //    namesList.Add(x.ToString()); // Liste elemanlarını string'e çevirip ekliyoruz.
+                //});
+            }
+
+            return View(namesList);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            db.ListLeftPush(listKey, name); // ListRightPush ile listenin sonuna veri ekliyoruz. ListLeftPush ile listenin başına eklenir.
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteItem(string name)
+        {
+            db.ListRemoveAsync(listKey, name).Wait(); // Redis'ten liste elemanını siliyoruz. Geri dönüş değeriyle ilgilenmediğimiz için async metodu wait ediyoruz.
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteFirstItem()
+        {
+            db.ListLeftPop(listKey); // ListLeftPop ile listenin başındaki elemanı siliyoruz. ListRightPop ile listenin sonundan siliyor.
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ StackExchangeRedisApp/Controllers/SetTypeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using StackExchangeRedisApp.Services;
+
+namespace StackExchangeRedisApp.Controllers
+{
+    public class SetTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+
+        private readonly IDatabase db;
+
+        private string listKey = "hashnames"; // Redis'te kullanacağımız anahtar.
+        public SetTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(0); //Redis 1.veritabanına bağlanıyoruz.
+        }
+        public IActionResult Index()
+        {
+            HashSet<string> namesList = new HashSet<string>(); //Hashset olarak kaydedince sırası önemli değil random kaydeder.
+
+            if (db.KeyExists(listKey))
+            {
+                db.SetMembers(listKey).ToList().ForEach(x =>
+                {
+                    namesList.Add(x.ToString()); // Liste elemanlarını string'e çevirip ekliyoruz.
+                });
+            }
+
+            return View(namesList);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            //if(!db.KeyExists(listKey)) // Redis'te anahtar var mı kontrol ediyoruz. İlgili anahtar yoksa süreyi 5 dk ayarlıyoruz. Bu kontrolü yapmazsak Add methoduna her girdiğinde 5 dk yenilenir.
+            //{
+            //    db.KeyExpire(listKey, DateTime.Now.AddMinutes(5)); // Redis'te anahtarın süresini 5 dakika olarak ayarlıyoruz.
+            //}
+
+            db.KeyExpire(listKey, DateTime.Now.AddMinutes(5)); // Redis'te anahtarın süresini 5 dakika olarak ayarlıyoruz.
+
+            db.SetAdd(listKey, name); //SetAdd ile listeye veri eklenir.
+
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DeleteItem(string name)
+        {
+            await db.SetRemoveAsync(listKey, name); // Redis'ten liste elemanını siliyoruz.
+            return RedirectToAction("Index");
+        }
+    }
+}
